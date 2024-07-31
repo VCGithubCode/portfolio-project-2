0d82c305c2875aa60cf8f8c18f9149fda57a9a73
VCGithubCode <vernellclark@hotmail.com>
2024-07-31 17:27:43 +0200

    chore: add docstrings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
+/**
+ * This script initializes a calculator and handles various events and functions related to the calculator.
+ * The calculator allows the user to calculate their age, the year they were born, and the date difference between two events.
+ * It also includes functions to toggle a modal, handle radio button changes, reset the calculator state, display questions based on the selected calculator type,
+ * hide prompts, show the recalculate button, run the calculator based on the selected calculator type, and handle various event listeners.
+ * The script also includes references to HTML elements and classes for manipulation and display purposes.
+ */
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize the calculator
   const initialCalculatorType = "HowOldAmIQuestion";
   runCalculator(initialCalculatorType);
+
   const modal = document.getElementById("instructionModal");
   const btn = document.getElementById("instructionButton");
   const span = document.querySelector(".close");
